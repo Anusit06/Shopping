246b0df1ef442a651589a913b82bc2ffd3fc6e46
Anusit06 <anusit.he@ku.th>
2025-08-03 14:07:01 +0700

    Add files via upload

--- Lab6/Lib/PricingService.java
+++ Lab6/Lib/PricingService.java
@@ -1,18 +1,5 @@
 package Lib;
 
-import java.util.ArrayList;
-
-import Lib.Discount.DefaulyPricingStrategy;
-import Lib.Discount.DiscountStrategy;
-
 public class PricingService {
-    public record StrategyRule(String sku, DiscountStrategy strategy) {}
-
-    private final ArrayList<StrategyRule> strategies = new ArrayList<>();
-
-    private final DiscountStrategy defaultStrategy = new DefaulyPricingStrategy();
-
-    /**
-     * ลงทะเบียนกลยุทธ์
-     */
+    
 }
